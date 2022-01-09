2b10a90e24679959ff7849e926c911473047b9b7
AyaOb <Skilpro2162@gmail.com>
2022-01-09 18:58:41 +0900

    java practice: variables and types

---
+++ Basic/Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String name = "Ooba Ayako";
+		System.out.println(name);
+		name = "song";
+		System.out.println(name);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
---
+++ Basic/Chapter05.java
@@ -0,0 +1,22 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 1L;
+		System.out.println(l);
+		double d = 1.9753;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+
+		float f = 1.753f;
+		System.out.println(f);
+		Integer i = 10;
+		System.out.println(i);
+		Character c = 'c';
+		System.out.println(c);
+
+		int[] array = {1,3,5,7};
+		System.out.println(array[3]);
+
+	}
+
+}
