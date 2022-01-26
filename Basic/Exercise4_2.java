06fad1fd9485341c1e74767616a91a97929b1fb4
AyaOb <Skilpro2162@gmail.com>
2022-01-26 19:11:21 +0900

    [Finish]Javaスッキリ入門:第4章

---
+++ Basic/Exercise4_2.java
@@ -0,0 +1,12 @@
+public class Exercise4_2 {
+	public static void main(String[] args) {
+		int[] moneylist = { 121902, 8302, 55100 };
+		for (int i = 0; i < moneylist.length; i++) {
+			System.out.println(moneylist[i]);
+		}
+
+		for(int money: moneylist) {
+			System.out.println(money);
+		}
+	}
+}
