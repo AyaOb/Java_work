81983537f200998e97f72f8a15f3142c47f3b1a3
AyaOb <Skilpro2162@gmail.com>
2022-01-29 18:45:54 +0900

    [Finish]Javaスッキリ入門第6章

---
+++ Basic/comment/Exercise6_Kouhan.java
@@ -0,0 +1,14 @@
+package comment;
+
+public class Exercise6_Kouhan {
+	public static void callDeae() {
+		System.out.println("えぇい、こしゃくな。曲者だ！であえい！");
+	}
+
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん。もういいでしょう。");
+		System.out.println("この紋所が目にはいらぬか！");
+		Thread.sleep(3000);
+		Exercise6_Zenhan.doTogame();
+	}
+}
---
+++ Basic/comment/Exercise6_Zenhan.java
@@ -0,0 +1,11 @@
+package comment;
+
+public class Exercise6_Zenhan {
+	public static void doWarusa() {
+		System.out.println("きなこでござる。食えませんがの。");
+	}
+
+	public static void doTogame() {
+		System.out.println("この老いぼれの目はごまかせませんぞ。");
+	}
+}
