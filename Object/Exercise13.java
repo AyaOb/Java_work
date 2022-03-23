4662e39dbb3ec776ad0a0e4c7311dd60eb7df5da
AyaOb <Skilpro2162@gmail.com>
2022-03-23 18:26:44 +0900

    [Finish]Javaスッキリ入門第13章:多態性

---
+++ Object/Exercise13.java
@@ -0,0 +1,13 @@
+
+public class Exercise13 {
+
+	public static void main(String[] args) {
+		Y[] array = new Y[2];
+		array[0] = new A();
+		array[1] = new B();
+		for (Y y : array) {
+			y.b();
+		}
+	}
+
+}
